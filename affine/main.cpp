bb489c032377ad5c044fa8a0aeda438b48aee644
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2021-05-15 06:04:44 +0900

    modified:   affine/main.cpp

--- affine/main.cpp
+++ affine/main.cpp
@@ -7,65 +7,88 @@
 using namespace cv;
 using namespace std;
 
-void reverseMatrix(vector<vector<double>> vec) {
-  int size = vec.size();
-  double inv_a[size][size]; //ここに逆行列が入る
+const double PI = 3.14159265358979323846;
 
-  double *a = vec[0].data();
-  //単位行列を作る
+vector<vector<double>> reverse(vector<vector<double>> vec) {
+  int size = vec.size();
+  // 単位行列作成
+  vector<vector<double>> inv_a(size, vector<double>(size, 0));
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) {
       inv_a[i][j] = (i == j) ? 1.0 : 0.0;
     }
   }
+
+  //掃き出し法
   for (int i = 0; i < size; i++) {
-    double buf = 1 / a[i][i];
+    double buf = 1 / vec[i][i];
     for (int j = 0; j < size; j++) {
-      a[i][j] *= buf;
+      vec[i][j] *= buf;
       inv_a[i][j] *= buf;
     }
     for (int j = 0; j < size; j++) {
       if (i != j) {
-        buf = a[j][i];
+        buf = vec[j][i];
         for (int k = 0; k < size; k++) {
-          a[j][k] -= a[i][k] * buf;
+          vec[j][k] -= vec[i][k] * buf;
           inv_a[j][k] -= inv_a[i][k] * buf;
         }
       }
     }
   }
+  return inv_a;
 }
 
-void createMatrix(vector<vector<double>> &vec, int angle, int x, int y) {
-  vec.push_back({cos(angle), -sin(angle), (double)x});
-  vec.push_back({sin(angle), cos(angle), (double)y});
-  vec.push_back({0, 0, 1});
-  return;
+double deg2rad(double degree) { return degree * PI / 180.0; }
+
+vector<vector<double>> createMatrix(double angle, int x, int y) {
+  vector<vector<double>> vec{{cos(angle), -sin(angle), (double)x},
+                             {sin(angle), cos(angle), (double)y},
+                             {0, 0, 1}};
+  return vec;
 }
 
 int main(int argc, char *argv[]) {
-
   string path = "./sample.jpg";
   Mat src, gray;
   src = imread(path);
 
   cvtColor(src, gray, COLOR_RGB2GRAY);
-
   Mat dst(gray.size(), gray.type());
   //
-  vector<vector<double>> matrix;
-  createMatrix(matrix, 45, 0, 0);
-  reverseMatrix(3, matrix);
+  vector<vector<double>> matrix = createMatrix(deg2rad(1), 0, 0);
+  for (int r = 0; r < matrix.size(); r++) {
+    for (int c = 0; c < matrix[0].size(); c++) {
+      cout << matrix[r][c] << endl;
+    }
+  }
 
-  for (int c = 0; c < gray.cols; c++) {
-    for (int r = 0; r < gray.rows; r++) {
-      unsigned char pt = gray.at<unsigned char>(r, c);
-      int idx = r * dst.step + c * dst.elemSize();
-      int dstPt = pt - 40;
-      if (dstPt < 0) {
-        dstPt = 0;
+  cout << "type:" << gray.size() << " type:" << gray.type() << endl;
+  cout << "rows:" << gray.rows << " cols:" << gray.cols << endl;
+  cout << "step:" << gray.step << " ele:" << gray.elemSize();
+  cout << " chan:" << gray.channels() << endl;
+
+  vector<vector<double>> rm = reverse(matrix);
+  for (int r = 0; r < rm.size(); r++) {
+    for (int c = 0; c < rm[0].size(); c++) {
+      cout << rm[r][c] << ",";
+    }
+    cout << endl;
+  }
+  cout << endl;
+
+  for (int r = 0; r < gray.rows; r++) {
+    for (int c = 0; c < gray.cols; c++) {
+      double refX = rm[0][0] * c + rm[0][1] * r;
+      double refY = rm[1][0] * c + rm[1][1] * r;
+      int rX = (int)(refX + 0.5);
+      int rY = (int)(refY + 0.5);
+      unsigned char val = 0;
+      if (rX >= 0 && rY >= 0 && rX < gray.cols && rY < gray.rows) {
+        val = gray.at<unsigned char>(rY, rX);
       }
-      dst.data[idx] = (unsigned char)dstPt;
+      int idx = r * gray.step + c;
+      dst.data[idx] = val;
     }
   }
   imshow("gray", gray);
